549bd95699adc95d8b2bb51b4763820c09c14e96
unknown <davicoelho.dev@gmail.com>
2024-07-22 10:09:09 -0300

    intro estruturas de dados

---
+++ Cap2-Estruturas-Nativas/intro.py
@@ -0,0 +1,23 @@
+# !!! INTRODUÇÃO ÀS ESTRUTURAS NATIVAS DE DADOS DE PYTHON !!!
+
+
+# Uma introdução breve sobre o assunto de estruturas será introduzido nesse capítulo. No caso, uma estrutura de dados nada mais é do que uma forma de organizar dados dentro de um programa e do próprio computador. 
+
+# Python possui algumas estruturas de dados já prontas da própria linguagem, como listas, dicionários, tuplas e conjuntos.
+
+# É importante frizar a relevância do conceito de estruturas no mundo da computação.
+# Como vimos até o momento, dentro do código temos entidades que representam certos conceitos, como condições, repetições, etc. Essas entidades são conhecidas como "estruturas de controle", ou seja, elas são responsáveis por definir a estrutura do código fonte em si, e também do controle da leitura feita pelo interpretador.
+
+# Em contraste com estruturas de controle, que compõem o próprio código, temos outros elementos que compõem a organização de dados no programa: As estruturas de dados.
+
+# -> Uma estrutura de dados é uma organização lógica dos dados no qual o programa/algoritmo irá manipular. Podemos pensar em uma e.d. como um mini banco de dados, embutido na própria aplicação. Existem várias estruturas, aonde cada uma possui sua própria lógica e forma de guardar os dados.
+
+# Exemplo:
+
+# estrutura de lista(nativa python):
+lista1 = [4.90, 4.85, 4.60, 5.00, 5.10, 5.05, 4.95]
+
+# estrutura de dicionário(nativa python):
+dict1 = {"nome": "davi", "idade": 23, "profissão": "programador"}
+
+# entre outras estruturas que veremos em outro arquivo...
