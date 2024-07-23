e715d4ebf8bec3bb9585468593b6bbee80b7bd39
unknown <davicoelho.cont@gmail.com>
2024-07-23 20:01:38 -0300

    conjuntos

---
+++ Cap2-Estruturas-Nativas/conjuntos.py
@@ -0,0 +1,25 @@
+# !!! INTRODUÇÃO À CONJUNTOS !!!
+
+# -> A estrutura de conjuntos em Python são uma representação computacional dos conjuntos matemáticos. Isso significa que temos as operações clássicas que envolvem conjuntos na matemática, como união, diferença, intersecção, etc.
+
+# Eis um exemplo de um conjunto:
+conjunto1 = {1, 2, 3, 4, 5}
+conjunto2 = {6, 7, 8, 9, 10}
+
+# Operação de união é feita através do operador |:
+uniao = conjunto1 | conjunto2
+print(uniao)
+
+# Operação de diferença é feita através do operador -:
+diferenca = conjunto1 - conjunto2
+print(diferenca)
+
+# Operação de intersecção é feita através do operador &:
+interseccao = conjunto1 & conjunto2
+print(interseccao)
+
+
+# Assim como as outras estruturas de dados, os sets(conjuntos) possuem suas próprias funções e operações(como vimos acima).
+
+# Como não é viável apresentar todas as funcionalidades de cada estrutura de dados, além do fato de que essas anotações são apenas introduções, utilizar a documentação ou realizar pesquisas mais aprofundadas na internet é mais recomendado.
+
